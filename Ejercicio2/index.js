e273d167c479ba32f338af609dc6a4ec2ab11401
Wilson Jerez <wil.renovation@gmail.com>
2022-09-16 14:15:25 -0400

    Agregando ejercicio de la unidad 2

---
+++ Ejercicio2/index.js
@@ -0,0 +1,13 @@
+var nombre="Wilson Jerez";
+var edad=36;
+var desarrollador=true;
+var fechaNacimiento=new Date(1985,09,05);
+var libroFavorito={
+    titulo:"La Vaca",
+    autor:"Camilo Cruz",
+    fecha:new Date(1999,8,16),
+    url:"http://courseware.url.edu.gt/Facultades/Facultad%20de%20Teolog%C3%ADa/Magis%20Landivariano%20Material/Libros/Libro%20La%20VACA.pdf"
+}
+
+var lista=[nombre,edad,desarrollador,fechaNacimiento,libroFavorito];
+console.log(lista);
