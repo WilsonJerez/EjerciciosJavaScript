9af840d14453a2fad74a015e85cd58f12dc98fb1
Wilson Jerez <wil.renovation@gmail.com>
2022-11-15 19:41:40 -0400

    Agregando ejercicio6 operaciones con numeros

---
+++ Ejercicio6-Numeros/Index.js
@@ -0,0 +1,13 @@
+let altura_cm=165;
+let altura_met=7.65;
+let peso=75.5;
+let alturaRedondeada=Math.ceil(altura_met);
+let pesoRedondeado=Math.floor(peso);
+let maxValor=Number.MAX_VALUE+1===Number.MAX_VALUE;
+
+console.log(altura_cm);
+console.log(altura_met);
+console.log(peso);
+console.log(alturaRedondeada);
+console.log(pesoRedondeado);
+console.log(maxValor);
