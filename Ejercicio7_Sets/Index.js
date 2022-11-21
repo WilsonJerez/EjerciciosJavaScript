fb8a2e14d8e6c3d5d42b8c3c5bddd4e57136a612
Wilson Jerez <wil.renovation@gmail.com>
2022-11-21 18:02:29 -0400

    Añadiendo ejercicio de sets

---
+++ Ejercicio7_Sets/Index.js
@@ -0,0 +1,10 @@
+let lista=["Walter","Arcelia","Nila","Teresa","Wilson"];
+let miSet=new Set(lista);
+console.log(miSet);
+//modificando set
+miSet.add("Wilson");
+console.log(miSet);
+//añdiendo javascript
+miSet.add("JavaScript");
+console.log(miSet);
+
