a4654972e8086bb193f2867ae7442d8660e882a7
Wilson Jerez <wil.renovation@gmail.com>
2022-11-22 10:00:20 -0400

    Añadiendo ejercicio fechas

---
+++ Ejercicio7_Sets/Fechas.js
@@ -0,0 +1,12 @@
+let fechaHoy=new Date();
+let miCumple=new Date(1985,09,05);
+let esMayor=(fechaHoy>miCumple);
+console.log(esMayor);
+let diaNacimiento=miCumple.getDate();
+let mesNacimiento=miCumple.getMonth();
+let anyoNacimiento=miCumple.getFullYear();
+console.log(diaNacimiento);
+console.log(mesNacimiento);
+console.log(anyoNacimiento);
+
+
