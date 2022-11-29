528f9240b7aff992392a145cb8a862e0588c2134
Wilson Jerez <wil.renovation@gmail.com>
2022-11-28 21:32:52 -0400

    añadiendo ejercicios 10

---
+++ Ejercicio10_Modulos/index.js
@@ -0,0 +1,7 @@
+import{suma,multiplica} from "./Controller.js";
+import chalk from "chalk";
+
+const reSuma=suma(6,8);
+console.log(reSuma);
+const reMultiplica=multiplica(2,8);
+console.log(chalk.green(reMultiplica));
